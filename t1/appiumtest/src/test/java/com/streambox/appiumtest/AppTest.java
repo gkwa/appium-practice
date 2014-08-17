10e1f3342a00387fd45b0eda0fe9139fdd3f77d1
taylormonacelli <tailor@u.washington.edu>
2014-08-17 15:52:45 -0700

    Get working with iPhone

--- t1/appiumtest/src/test/java/com/streambox/appiumtest/AppTest.java
+++ t1/appiumtest/src/test/java/com/streambox/appiumtest/AppTest.java
@@ -4,6 +4,16 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.URL;
+import java.net.MalformedURLException;
+import org.openqa.selenium.WebDriver;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Unit test for simple App.
  */
@@ -33,6 +43,30 @@ public class AppTest
      */
     public void testApp()
     {
-        assertTrue( true );
+		//		assertTrue( true );
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("appium-version", "1.0");
+		capabilities.setCapability("platformName", "iOS");
+		capabilities.setCapability("platformVersion", "7.1");
+		capabilities.setCapability("deviceName", "iPhone");
+		capabilities.setCapability("app", "/Users/demo/Downloads/streambox-app.3.0.14/meproiphone.app");
+		capabilities.setCapability("udid","eba88a92c6a33123b42450da634b243244ae282b");
+
+		AppiumDriver wd = null;
+		try {
+			wd = new AppiumDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		wd.findElement(By.name("Title")).clear();
+		wd.findElement(By.name("Title")).sendKeys("test");
+		wd.findElement(By.name("Location")).sendKeys("test");
+		wd.findElement(By.name("Login")).click();
+		// if alert, then ok the alert
+		//		wd.findElement(By.name("OK")).click();
+		try { Thread.sleep(5000); } catch(InterruptedException e) { }
+		wd.close();
+
     }
 }
