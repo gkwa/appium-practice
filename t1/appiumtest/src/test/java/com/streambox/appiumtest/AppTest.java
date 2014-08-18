66630a6de607d18412fcd622ebc5275e05710b56
taylormonacelli <tailor@u.washington.edu>
2014-08-17 22:56:40 -0700

    Test successful and successful login

--- t1/appiumtest/src/test/java/com/streambox/appiumtest/AppTest.java
+++ t1/appiumtest/src/test/java/com/streambox/appiumtest/AppTest.java
@@ -1,6 +1,10 @@
 package com.streambox.appiumtest;
 
-import junit.framework.Test;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+
+// import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
@@ -14,59 +18,74 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
+	private AppiumDriver wd;
+
     public AppTest( String testName )
     {
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    @Before
+    public void setUp() throws Exception {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-		//		assertTrue( true );
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability("appium-version", "1.0");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+		//		capabilities.setCapability("appium-version", "1.0");
 		capabilities.setCapability("platformName", "iOS");
 		capabilities.setCapability("platformVersion", "7.1");
 		capabilities.setCapability("deviceName", "iPhone");
 		capabilities.setCapability("app", "/Users/demo/Downloads/streambox-app.3.0.14/meproiphone.app");
 		capabilities.setCapability("udid","eba88a92c6a33123b42450da634b243244ae282b");
-
-		AppiumDriver wd = null;
-		try {
-			wd = new AppiumDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
+        wd = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 		wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-		wd.findElement(By.name("Title")).clear();
-		wd.findElement(By.name("Title")).sendKeys("test");
-		wd.findElement(By.name("Location")).sendKeys("test");
-		wd.findElement(By.name("Login")).click();
-		// if alert, then ok the alert
-		//		wd.findElement(By.name("OK")).click();
-		try { Thread.sleep(5000); } catch(InterruptedException e) { }
-		wd.close();
+    }
 
+	@Test
+    public void testApp()
+    {
+		wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		Boolean weAreLoggedOutNow = wd.findElements(By.name("Login")).size() > 0;
+		if(!weAreLoggedOutNow) {
+			wd.findElement(By.name("Settings Button   Neutral")).click();
+			wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[7]/UIAStaticText[1]")).click();
+			try { Thread.sleep(1000); } catch(InterruptedException e) { }
+		}else{
+			wd.findElement(By.name("Title")).clear();
+			wd.findElement(By.name("Title")).sendKeys("m");
+			wd.findElement(By.name("Location")).sendKeys("d");
+			wd.findElement(By.name("Login")).click();
+			Boolean alertOccured = wd.findElements(By.name("OK")).size() > 0;
+			if(alertOccured) {
+				wd.switchTo().alert().accept();
+			}
+		}
+    }
+
+	@Test
+    public void testApp2()
+    {
+		wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		Boolean weAreLoggedOutNow = wd.findElements(By.name("Login")).size() > 0;
+		if(!weAreLoggedOutNow) {
+			wd.findElement(By.name("Settings Button   Neutral")).click();
+			wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[7]/UIAStaticText[1]")).click();
+			try { Thread.sleep(1000); } catch(InterruptedException e) { }
+		}else{
+			wd.findElement(By.name("Title")).clear();
+			wd.findElement(By.name("Title")).sendKeys("test");
+			wd.findElement(By.name("Location")).sendKeys("d");
+			wd.findElement(By.name("Login")).click();
+			Boolean alertOccured = wd.findElements(By.name("OK")).size() > 0;
+			if(alertOccured) {
+				wd.switchTo().alert().accept();
+			}
+		}
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        wd.quit();
     }
 }
